879af07fa8b8baec0da7ba154310dab1a07912af
Emree Jensen <149439495+emreejensen@users.noreply.github.com>
2025-05-19 21:58:53 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $hostname = "localhost";
-$username = "ejensend_ej";
-$password = "W3bT3ch@2002";
-$dbname = "ejensend_4450project1";
+$username = "";
+$password = "";
+$dbname = "";
 
 // Use mysqli_connect to connect to the database
 $dbc = mysqli_connect($hostname, $username, $password, $dbname) OR die("Cannot Connect to Database, ERROR...");
